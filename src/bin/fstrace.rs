6cfd476769d36476e4e1ebfb28824b82cf1c61fe
Tingmao Wang <m@maowtm.org>
2026-03-16 23:40:17 +0000

    Add some request filtering capabilities to fstrace

--- src/bin/fstrace.rs
+++ src/bin/fstrace.rs
@@ -1,10 +1,14 @@
-use log::{error, info};
-use std::env;
+use libturnstile::fs::{ForeignFd, FsOperation, FsTarget, RwxPermission};
+use log::{debug, error, info, warn};
+use std::collections::HashSet;
+use std::ffi::{OsStr, OsString};
 use std::io::Write;
 use std::os::fd::FromRawFd;
+use std::os::unix::ffi::{OsStrExt, OsStringExt};
 use std::process::Command;
 use std::sync::{Arc, OnceLock};
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+use std::{env, io};
 
 use clap::Parser;
 use libturnstile::{AccessRequestError, Operation, TurnstileTracer};
@@ -31,9 +35,142 @@ struct Cli {
 	#[arg(long = "rwx")]
 	rwx: bool,
 
+	/// Only print attempted writes
+	#[arg(short = 'w', long = "only-writes")]
+	only_writes: bool,
+
+	/// Exclude paths with any of these prefixes.  Relative paths are resolved to their absolute paths.
+	#[arg(short = 'e', long = "exclude", value_name = "PREFIX")]
+	exclude: Vec<OsString>,
+
+	/// Exclude paths with any of these prefixes if the request is read-only.  Relative paths are resolved to their absolute paths.
+	#[arg(short = 'E', long = "exclude-ro", value_name = "PREFIX")]
+	exclude_ro: Vec<OsString>,
+
+	/// Include only paths with any of these prefixes.  Relative paths are resolved to their absolute paths.
+	#[arg(short = 'i', long = "include", value_name = "PREFIX")]
+	include: Vec<OsString>,
+
+	/// Exclude read requests under any paths that are in PATH
+	#[arg(long = "exclude-read-on-env-path")]
+	exclude_read_on_env_path: bool,
+
+	/// Exclude readlink requests for any parents of any excluded paths (i.e. what realpath does)
+	#[arg(long = "exclude-readlink-for-parents-of-excluded-paths")]
+	exclude_readlink_for_parents_of_excluded_paths: bool,
+
+	/// Only print the first access for each file
+	#[arg(short = 'u', long = "unique")]
+	unique: bool,
+
 	/// Program to trace and its arguments
 	#[arg(required = true)]
 	command: Vec<String>,
+}
+
+impl Cli {
+	fn need_path(&self, request_is_ro: bool) -> bool {
+		if self.unique {
+			return true;
+		}
+		if !self.exclude.is_empty() || !self.include.is_empty() {
+			return true;
+		}
+		if request_is_ro && !self.exclude_ro.is_empty() {
+			return true;
+		}
+		false
+	}
+}
+
+fn path_has_prefix(path: &OsString, prefix: &OsString) -> bool {
+	path.as_encoded_bytes()
+		.starts_with(prefix.as_encoded_bytes())
+}
+
+fn filter(
+	cli: &Cli,
+	fs_op: &FsOperation,
+	rwx: &RwxPermission,
+	unique_hs: &mut HashSet<OsString>,
+) -> io::Result<bool> {
+	if cli.only_writes && !rwx.write {
+		return Ok(false);
+	}
+	if cli.need_path(!rwx.write) {
+		let rough_target_path = match rwx.target.realpath() {
+			Ok(p) => p,
+			Err(e) => {
+				let dfd = rwx.target.dfd();
+				let mut p = match dfd.readlink() {
+					Err(e) => {
+						error!("Child process passed an invalid file descriptor: {}", e);
+						return Ok(true);
+					}
+					Ok(p) => p,
+				}
+				.into_encoded_bytes();
+				if p.last().copied() != Some(b'/') && !rwx.target.path().is_empty() {
+					p.push(b'/');
+				}
+				p.extend_from_slice(rwx.target.path().to_bytes());
+				OsString::from_vec(p)
+			}
+		};
+		if cli
+			.exclude
+			.iter()
+			.any(|prefix| path_has_prefix(&rough_target_path, prefix))
+		{
+			return Ok(false);
+		}
+		if rwx.write
+			&& cli
+				.exclude_ro
+				.iter()
+				.any(|prefix| path_has_prefix(&rough_target_path, prefix))
+		{
+			return Ok(false);
+		}
+		if !cli.include.is_empty()
+			&& !cli
+				.include
+				.iter()
+				.any(|prefix| path_has_prefix(&rough_target_path, prefix))
+		{
+			return Ok(false);
+		}
+		if let FsOperation::FsReadlink(_) = fs_op
+			&& cli.exclude_readlink_for_parents_of_excluded_paths
+		{
+			for excluded_path in cli.exclude.iter().chain(cli.exclude_ro.iter()) {
+				if path_has_prefix(excluded_path, &rough_target_path) {
+					return Ok(false);
+				}
+			}
+		}
+		if cli.unique {
+			return Ok(unique_hs.insert(rough_target_path));
+		}
+	}
+	Ok(true)
+}
+
+fn turn_into_absolute_paths(paths: &mut [OsString]) -> io::Result<()> {
+	for path in paths {
+		match std::fs::canonicalize(&path) {
+			Ok(realpath) => *path = realpath.into_os_string(),
+			Err(e) => {
+				if path.as_encoded_bytes().starts_with(b"/") {
+					warn!("Error opening path {:?}: {}. Using as-is.", path, e);
+				} else {
+					error!("Error opening path {:?}: {}", path, e);
+					return Err(e);
+				}
+			}
+		}
+	}
+	Ok(())
 }
 
 #[derive(Debug)]
@@ -45,7 +182,7 @@ pub struct Context {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	common::init_logger();
 
-	let cli = Cli::parse();
+	let mut cli = Cli::parse();
 
 	let mut output: Box<dyn Write + Send> = match &cli.output {
 		Some(path) => Box::new(
@@ -64,6 +201,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	let program = &cli.command[0];
 	let args = &cli.command[1..];
+
+	turn_into_absolute_paths(&mut cli.exclude)?;
+	turn_into_absolute_paths(&mut cli.include)?;
+
+	if cli.exclude_read_on_env_path {
+		if let Some(env_path) = env::var_os("PATH") {
+			for p in env_path.as_encoded_bytes().split(|&b| b == b':') {
+				let p = OsStr::from_bytes(p);
+				if !p.is_empty() {
+					let rp = std::fs::canonicalize(p).map(|s| s.into_os_string());
+					let rp = rp.unwrap_or_else(|e| {
+						warn!("Error opening path {:?} from PATH: {}. Using as-is.", p, e);
+						p.to_owned()
+					});
+					cli.exclude.push(rp);
+				}
+			}
+		} else {
+			warn!(
+				"Environment variable PATH is not set, so --exclude-read-on-env-path has no effect"
+			);
+		}
+	}
+
 	let mut cmd = Command::new(program);
 	cmd.args(args);
 
@@ -73,9 +234,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let context_for_thread = Arc::clone(&context);
 	std::thread::spawn(move || {
 		let context = context_for_thread;
+		let mut unique_hs = HashSet::new();
 		loop {
 			match context.tracer.yield_request() {
 				Ok(Some((access_request, mut ctx))) => {
+					let fs_op = match access_request.operation() {
+						Operation::FsOperation(fs_op) => fs_op,
+						_ => continue,
+					};
+					let rwxp = fs_op.as_rwx_permissions();
+					let mut any_true = false;
+					for f in rwxp.iter() {
+						match filter(&cli, fs_op, f, &mut unique_hs) {
+							Ok(true) => {
+								any_true = true;
+								break;
+							}
+							Ok(false) => {}
+							Err(e) => {
+								error!("Error evaluating filter on access request: {}", e);
+							}
+						}
+					}
+					if !any_true {
+						if let Err(e) = ctx.send_continue() {
+							error!("error sending continue response: {}", e);
+						}
+						continue;
+					}
 					if cli.timestamps {
 						let now = SystemTime::now()
 							.duration_since(SystemTime::UNIX_EPOCH)
@@ -91,24 +277,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 					let comm = std::fs::read(format!("/proc/{}/comm", pid))
 						.map(|r| r.trim_ascii().escape_ascii().to_string())
 						.unwrap_or_else(|_| String::from("???"));
-					if let Err(e) = match access_request.operation() {
-						Operation::FsOperation(fs_op) if cli.rwx => {
-							let rwxp = fs_op.as_rwx_permissions();
-							match &rwxp[..] {
-								[p] => writeln!(output, "{}[{}] {}", comm, pid, p),
-								[p1, p2] => {
-									writeln!(output, "{}[{}] {}; {}", comm, pid, p1, p2)
-								}
-								_ => panic!(
-									"unexpected number of permissions returned by as_rwx_permissions()"
-								),
+					let write_res = if cli.rwx {
+						match &rwxp[..] {
+							[p] => writeln!(output, "{}[{}] {}", comm, pid, p),
+							[p1, p2] => {
+								writeln!(output, "{}[{}] {}; {}", comm, pid, p1, p2)
 							}
+							_ => panic!(
+								"unexpected number of permissions returned by as_rwx_permissions()"
+							),
 						}
-						Operation::FsOperation(fs_op) => {
-							writeln!(output, "{}[{}] {}", comm, pid, fs_op)
-						}
-						_ => Ok(()),
-					} {
+					} else {
+						writeln!(output, "{}[{}] {}", comm, pid, fs_op)
+					};
+					if let Err(e) = write_res {
 						error!("error writing to log: {}", e);
 						return;
 					}
@@ -118,6 +300,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 				}
 				Ok(None) => {}
 				Err(e) => {
+					std::thread::sleep(Duration::from_millis(20));
 					if let Some(pidfd) = context.pidfd.get() {
 						match pidfd.is_alive() {
 							Ok(alive) => {
